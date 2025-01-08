b706969a4eb1e5a24a35e3ad329f262c22994c9c
Eltonn0816 <elton.lojdkvist@elev.ga.lbs.se>
2025-01-08 10:45:38 +0100

    Gjorde HP scene och fixade namn

---
+++ Programveckor Spel Lords 8/Assets/Scenes/Scripts/Player health.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int playerhealth; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerhealth = 10; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerhealth < 1)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
