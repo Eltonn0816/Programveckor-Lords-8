3d1b20e403e39ecde1ef9dc6466c69f29ac5b1b5
Eltonn0816 <elton.lojdkvist@elev.ga.lbs.se>
2025-01-09 09:16:24 +0100

    Movement added and put the guide sprite in unity

--- Programveckor Spel Lords 8/Assets/diffrent scripts/player script/Movement script player.cs
+++ Programveckor Spel Lords 8/Assets/diffrent scripts/player script/Movement script player.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Playermovement : MonoBehaviour
 {
+    Rigidbody2D rb; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        //needs rigidbody  
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        rb.velocity = new Vector2(0, 0); 
+        if(Input.GetKey(KeyCode.D) && transform.position.x < 8.45) 
+        {
+            rb.velocity = new Vector2(5, 0);
+        }
+        if(Input.GetKey(KeyCode.A) && transform.position.x > -8.45)
+        {
+            rb.velocity = new Vector2(-5, 0);
+        }
+        if(Input.GetKey(KeyCode.S) && transform.position.y > -4.45)
+        {
+            rb.velocity = new Vector2(0, -5); 
+        }
+        if (Input.GetKey(KeyCode.W) && transform.position.y < 4.45)
+        {
+            rb.velocity = new Vector2(0, 5); 
+        }
     }
 }
