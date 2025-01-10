5b771528a79b300a9a03905e05f56fd4e89d605d
Eltonn0816 <elton.lojdkvist@elev.ga.lbs.se>
2025-01-10 11:24:41 +0100

    Changed enemy script name

--- Programveckor Spel Lords 8/Assets/Scripts/Enemy scripts/Passive enemy.cs
+++ Programveckor Spel Lords 8/Assets/Scripts/Enemy scripts/Passive enemy.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
-public class Enemy : MonoBehaviour
+public class passiveEnemy : MonoBehaviour
     
 {
     Rigidbody2D rb; public float targetTime = 0f;
@@ -21,6 +21,7 @@ public class Enemy : MonoBehaviour
         targetTime += Time.deltaTime;
         if (targetTime >= 1f)
         {
+            
             rb.velocity = new Vector2(Random.Range(-3, 3), Random.Range(-3, 3));
             if(targetTime <= 3)
             {
