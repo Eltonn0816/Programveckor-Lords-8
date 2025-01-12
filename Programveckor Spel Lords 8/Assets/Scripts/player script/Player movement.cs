e5ab40fceba82323b32241c254fb457480cfe79f
Lord1Murad <Muradiksweden@gmail.com>
2025-01-12 16:29:12 +0100

    fixade walking (behöver left down and right down sen är det klart) + adda normolised speed

--- Programveckor Spel Lords 8/Assets/Scripts/player script/Player movement.cs
+++ Programveckor Spel Lords 8/Assets/Scripts/player script/Player movement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -19,45 +20,54 @@ public class Playermovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
        // needs rigidbody  
         animator = GetComponent<Animator>();
-       
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        rb.velocity = new Vector2(0, 0); 
-        if(Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
+
+        Vector2 movement = Vector2.zero;
+        float speed = 5f; 
+
+        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.D))
         {
             animator.Play("right up knight walk");
+            movement = new Vector2(1, 1);
         }
-        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W))
+        else if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W))
         {
             animator.Play("walk left up");
+            movement = new Vector2(-1, 1);
         }
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            rb.velocity += new Vector2(5, 0);
+            movement = new Vector2(1, 0);
             animator.Play("walk right knight");
         }
-        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        else if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            rb.velocity += new Vector2(-5, 0);
+            movement = new Vector2(-1, 0);
             animator.Play("knight walking left");
         }
-        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
-            rb.velocity += new Vector2(0, -5);
+            movement = new Vector2(0, -1);
             animator.Play("walking back knight");
         }
-        if (Input.GetKey(KeyCode.W)  || Input.GetKey(KeyCode.UpArrow))
+        else if (Input.GetKey(KeyCode.W)  || Input.GetKey(KeyCode.UpArrow))
         {
-            rb.velocity += new Vector2(0, 5);
+            movement = new Vector2(0, 1);
             animator.Play("walking farward");
         }
-       // if (!Input.GetKey(KeyCode.S) || !Input.GetKey(KeyCode.W) || !Input.GetKey(KeyCode.A) || !Input.GetKey(KeyCode.D) || !Input.GetKey(KeyCode.UpArrow) || !Input.GetKey(KeyCode.DownArrow) || !Input.GetKey(KeyCode.LeftArrow) || !Input.GetKey(KeyCode.RightArrow ))
-       //{
-       //     animator.Play("idel knight"); 
-       // }
-        
+        else 
+        {
+            animator.Play("idel knight"); 
+        }
+        if (movement != Vector2.zero)
+        {
+            movement = movement .normalized * speed; 
+        }
+        rb.velocity = movement;
     }
 }
