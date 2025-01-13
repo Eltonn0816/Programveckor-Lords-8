86d574a07fc4d34f0b9b12c2d7b36c17daeef4c4
Lord1Murad <Muradiksweden@gmail.com>
2025-01-13 08:51:01 +0100

    ,

--- Programveckor Spel Lords 8/Assets/idle.cs
+++ Programveckor Spel Lords 8/Assets/idle.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class idle : StateMachineBehaviour
 {
+    
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     //{
