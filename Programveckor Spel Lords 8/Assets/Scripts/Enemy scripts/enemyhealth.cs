5b33771f645c6583c78a517e7cf3b2850a6fec08
Eltonn0816 <elton.lojdkvist@elev.ga.lbs.se>
2025-01-14 01:05:44 +0100

    Gjorde folders och jobbar med attack men d funkar inte än

--- Programveckor Spel Lords 8/Assets/Scripts/Enemy scripts/enemyhealth.cs
+++ Programveckor Spel Lords 8/Assets/Scripts/Enemy scripts/enemyhealth.cs
@@ -5,17 +5,34 @@ using UnityEngine;
 public class EnemyHealth : MonoBehaviour
 {
     public float health = 100f;  
-    public float damageTaken = 40f; 
+    public float damageTaken = 35f; 
 
     // This method will be called when the enemy is hit
     public void TakeDamage(float damage)
     {
-        if (health <= 0)
+        health -= damage; 
+        if (health <= 1)
         {
             Die();  // Call the die method if health reaches 0
         }
     }
-
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        if (collision.gameObject.CompareTag("Weapon"))
+        {
+            TakeDamage(damageTaken);
+           
+        } 
+      
+    }
+    private void Update()
+    {
+        if (health <= 1)
+        {
+            Die();  // Call the die method if health reaches 0
+        }
+    }
     void Die()
     {
         Destroy(gameObject);  // Trigger death animation first
--- Programveckor Spel Lords 8/Assets/Scripts/player script/player combat.cs
+++ Programveckor Spel Lords 8/Assets/Scripts/player script/player combat.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class playercombat : MonoBehaviour
 {
+    public BoxCollider2D attackHitbox;
+    public float attackDuration = 0.1f;
+
+    private Vector2 rightPosition = new Vector2(1f, 0f); // Position when facing right
+    private Vector2 leftPosition = new Vector2(-1f, 0f); // Position when facing left
+
+    private bool isAttacking = false;
+    Animator animator;
+   
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+        if (Input.GetKey(KeyCode.D) && Input.GetKeyDown(KeyCode.Space) && !isAttacking)
+        {
+            animator.Play("1 click rv punch");
+            StartCoroutine(PerformAttack());
+        }
+        UpdateHitboxPosition();
+    }
+    void UpdateHitboxPosition()
+    {
+        // Detect the player's facing direction
+        float playerDirection = transform.localScale.x > 0 ? 1 : -1;
+        attackHitbox.offset = playerDirection > 0 ? rightPosition : leftPosition;
+    }
+
+    System.Collections.IEnumerator PerformAttack()
+    {
+        isAttacking = true; //enables attacking
+        attackHitbox.enabled = true; //enables hitbox
+        yield return new WaitForSeconds(attackDuration); //waits for the attack duration
+        attackHitbox.enabled = false; //disables hitbox
+        isAttacking = false; //disables attacking
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (attackHitbox.enabled && collision.CompareTag("Enemy"))
+        {
+          
+            EnemyHealth enemy = collision.GetComponent<EnemyHealth>();
+            if (enemy != null)
+            {
+                enemy.TakeDamage(35);
+            }
+        }
     }
 }
