44884c5b98b696cbfdce602f07a2601745aff502
Eltonn0816 <elton.lojdkvist@elev.ga.lbs.se>
2025-01-14 14:44:08 +0100

    Gameover scene

--- Programveckor Spel Lords 8/Assets/Scripts/Buttons.cs
+++ Programveckor Spel Lords 8/Assets/Scripts/Buttons.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class quitgame : MonoBehaviour
+public class buttonfunctions : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    public void exitgame()
+    {
+        Application.Quit();
+    }
+    public void tryagain()
+    {
+        SceneManager.LoadScene(0);
+    }
     // Update is called once per frame
     void Update()
     {
