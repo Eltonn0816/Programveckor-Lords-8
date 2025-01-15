f025c03dbb2c011ad7b5a4b9a055b6260934f6e5
Eltonn0816 <elton.lojdkvist@elev.ga.lbs.se>
2025-01-15 14:34:09 +0100

    viking walk animated and working

--- Programveckor Spel Lords 8/Assets/Scripts/Enemy scripts/Aggressive enemy.cs
+++ Programveckor Spel Lords 8/Assets/Scripts/Enemy scripts/Aggressive enemy.cs
@@ -7,6 +7,7 @@ public class Aggressive : MonoBehaviour
     public Transform player;
     public float speed = 4f;
     Animator animator;
+    private string currentanimation;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +17,31 @@ public class Aggressive : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        string newanimation = null; 
         Vector3 direction = (player.position - transform.position).normalized;
         transform.position += direction * speed * Time.deltaTime;
-        direction = direction.normalized * speed; 
-        // if(player.transform.position.x > transform.position.x) { animator.Play("vikingwalkRW"); }
+        direction = direction.normalized * speed;
+        if (Mathf.Abs(player.position.x - transform.position.x) > Mathf.Abs(player.position.y - transform.position.y))
+        {
+            if (player.transform.position.x > transform.position.x)
+            { newanimation = "vikingwalkRW"; }
+
+            else
+            { newanimation = "vikingwalkLW"; }
+        }
+        else
+        {
+            if (player.transform.position.y > transform.position.y)
+            { newanimation = "vikingwalkBW"; }
+
+            else
+            { newanimation = "vikingwalkFW"; }
+        }
+        
+        if(newanimation != null && newanimation != currentanimation)
+        {
+            animator.Play(newanimation);
+            currentanimation = newanimation;
+        }
     }
 }
