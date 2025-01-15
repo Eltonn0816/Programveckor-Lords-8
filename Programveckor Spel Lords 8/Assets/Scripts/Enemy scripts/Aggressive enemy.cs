def31c378b9a1f689f5b90286a209033e904600b
Eltonn0816 <elton.lojdkvist@elev.ga.lbs.se>
2025-01-15 13:52:28 +0100

    viking animations

--- Programveckor Spel Lords 8/Assets/Scripts/Enemy scripts/Aggressive enemy.cs
+++ Programveckor Spel Lords 8/Assets/Scripts/Enemy scripts/Aggressive enemy.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class Aggressive : MonoBehaviour
 {
     public Transform player;
-    public float speed = 4f; 
+    public float speed = 4f;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -18,5 +19,6 @@ public class Aggressive : MonoBehaviour
         Vector3 direction = (player.position - transform.position).normalized;
         transform.position += direction * speed * Time.deltaTime;
         direction = direction.normalized * speed; 
+        // if(player.transform.position.x > transform.position.x) { animator.Play("vikingwalkRW"); }
     }
 }
