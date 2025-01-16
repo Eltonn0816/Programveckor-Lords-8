1b6258c43d4ce18bb9363a936b10df9d688a5fca
Eltonn0816 <elton.lojdkvist@elev.ga.lbs.se>
2025-01-16 19:57:50 +0100

    viking animations and attack script wip

--- Programveckor Spel Lords 8/Assets/Scripts/Enemy scripts/Aggressive enemy.cs
+++ Programveckor Spel Lords 8/Assets/Scripts/Enemy scripts/Aggressive enemy.cs
@@ -8,23 +8,47 @@ public class Aggressive : MonoBehaviour
     public float speed = 4f;
     Animator animator;
     private string currentanimation;
+    private bool playerIsClose = false;
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
     }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsClose = true;
+        }
+        else { playerIsClose = false; }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsClose = false;
+        }
 
+    }
     // Update is called once per frame
     void Update()
     {
-        string newanimation = null; 
+       
+        string newanimation = null;
         Vector3 direction = (player.position - transform.position).normalized;
         transform.position += direction * speed * Time.deltaTime;
         direction = direction.normalized * speed;
         if (Mathf.Abs(player.position.x - transform.position.x) > Mathf.Abs(player.position.y - transform.position.y))
         {
+           
             if (player.transform.position.x > transform.position.x)
-            { newanimation = "vikingwalkRW"; }
+            {
+                if (playerIsClose = true)
+                {
+                    //play vikingattack right
+                }
+                newanimation = "vikingwalkRW";
+            }
 
             else
             { newanimation = "vikingwalkLW"; }
@@ -32,16 +56,31 @@ public class Aggressive : MonoBehaviour
         else
         {
             if (player.transform.position.y > transform.position.y)
-            { newanimation = "vikingwalkBW"; }
+            {
+                if (playerIsClose = true)
+                {
+                    newanimation = "vikingattackBW";
+                }
+                else { newanimation = "vikingwalkBW"; }
+            }
 
             else
-            { newanimation = "vikingwalkFW"; }
+            {
+
+                if (playerIsClose = true)
+                {
+                    newanimation = "vikingattackFW";
+                }
+                else { newanimation = "vikingwalkFW"; }
+            }
         }
-        
-        if(newanimation != null && newanimation != currentanimation)
+
+        if (newanimation != null && newanimation != currentanimation)
         {
             animator.Play(newanimation);
             currentanimation = newanimation;
         }
+
+
     }
 }
