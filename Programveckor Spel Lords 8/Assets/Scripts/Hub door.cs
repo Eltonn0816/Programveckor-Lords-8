f479ad3fad5ce63a54fb7804bb6831566e5e354f
Lord1Murad <Muradiksweden@gmail.com>
2025-01-21 23:22:29 +0100

    door lock and edited door script +chained door sprite

--- Programveckor Spel Lords 8/Assets/Scripts/Hub door.cs
+++ Programveckor Spel Lords 8/Assets/Scripts/Hub door.cs
@@ -6,21 +6,37 @@ using UnityEngine.SceneManagement;
 public class Enterdoorscript : MonoBehaviour
 {
 
+    public string doorID;
+    public string sceneToLoad;
+    public Sprite chaindDoorSprite;
+
+    private SpriteRenderer spriteRenderer;
+    private bool isDoorUsed;
+
     public bool playerIsClose;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (GameManagerScript.Instance.usedDoors.ContainsKey(doorID) && GameManagerScript.Instance.usedDoors[doorID])
+        {
+            isDoorUsed = true;
+            UpdateDoorAppearance();
+
+        }
+
     }
-    
+
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && playerIsClose)
+        if (Input.GetKeyDown(KeyCode.E) && playerIsClose && !isDoorUsed)
         {
-            SceneManager.LoadScene(1); //ändra scene nummer till level 1 när vi har den 
+            GameManagerScript.Instance.usedDoors[doorID] = true;
+            SceneManager.LoadScene(sceneToLoad);
         }
-        
+
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -28,5 +44,21 @@ public class Enterdoorscript : MonoBehaviour
         {
             playerIsClose = true;
         }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerIsClose = false;
+        }
+    }
+
+    private void UpdateDoorAppearance()
+    {
+        if (chaindDoorSprite != null)
+        {
+            spriteRenderer.sprite = chaindDoorSprite;
+        }
+        GetComponent<Collider2D>().enabled = false;
     }
 }
--- Programveckor Spel Lords 8/Assets/Scripts/doorToVikings.cs
+++ Programveckor Spel Lords 8/Assets/Scripts/doorToVikings.cs
@@ -5,21 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class doorToVikings : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string doorID;
+    public string sceneToLoad;
+    public Sprite chaindDoorSprite;
+
+    private SpriteRenderer spriteRenderer;
+    private bool isDoorUsed;
 
     public bool playerIsClose;
     // Start is called before the first frame update
     void Start()
     {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if(GameManagerScript.Instance.usedDoors.ContainsKey(doorID) && GameManagerScript.Instance.usedDoors[doorID])
+        {
+            isDoorUsed = true;
+            UpdateDoorAppearance();
+
+        }
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E) && playerIsClose)
+        if (Input.GetKeyDown(KeyCode.E) && playerIsClose && !isDoorUsed )
         {
-            SceneManager.LoadScene(2); //ändra scene nummer till level 1 när vi har den 
+            GameManagerScript.Instance.usedDoors[doorID] = true;
+            SceneManager.LoadScene(sceneToLoad);
         }
 
     }
@@ -29,5 +43,21 @@ public class doorToVikings : MonoBehaviour
         {
             playerIsClose = true;
         }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            playerIsClose = false;
+        }
+    }
+
+    private void UpdateDoorAppearance()
+    {
+        if(chaindDoorSprite != null)
+        {
+            spriteRenderer.sprite = chaindDoorSprite;
+        }
+        GetComponent<Collider2D>().enabled = false;
     }
 }
